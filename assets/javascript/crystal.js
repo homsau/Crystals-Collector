2fddc2973574c38f98841ce1ca2e59f12c048565
James Cowart <36057538+homsau@users.noreply.github.com>
2018-06-14 18:57:31 -0400

    Add files via upload

---
+++ assets/javascript/crystal.js
@@ -0,0 +1,114 @@
+$(document).ready(function() {
+    
+    // Creates variables
+    var randomNumber = Math.floor(Math.random()*101+19);
+    console.log(randomNumber);
+    var total = 0;
+    var wins = 0;
+    var losses = 0;
+    var green = Math.floor(Math.random()*11+1);
+    var blue = Math.floor(Math.random()*11+1);
+    var red = Math.floor(Math.random()*11+1);
+    var purple = Math.floor(Math.random()*11+1);
+    console.log(green, blue, red, purple);
+    var audio = new Audio('assets/audio/kakariko-village.mp3');
+    var rupeeSound = document.createElement("audio");
+        rupeeSound.src = "assets/audio/rupee.wav";
+        rupeeSound.volume = 1;
+        rupeeSound.autoPlay = false;
+        rupeeSound.preLoad = true;
+
+    //reset the game after completed and redeclare rupee random valuables
+    function reset() {
+        total = 0;
+        randomNumber = Math.floor(Math.random()*101+19);
+        green = Math.floor(Math.random()*11+1);
+        blue = Math.floor(Math.random()*11+1);
+        red = Math.floor(Math.random()*11+1);
+        purple = Math.floor(Math.random()*11+1);
+        $('#number').text(randomNumber);
+        $('#finalTotal').text(total);
+        console.log(green, blue, red, purple);
+        $('#wins').text("Wins: " + wins);
+        $('#losses').text("Losses: " + losses);
+    }
+    
+    // Play audio (town music on loop and ruppee sound once)
+    function noise() {
+        audio.play();
+        audio.loop = true;
+        $(".playSound").click(function() {
+            rupeeSound.play();
+        });
+    }
+    noise();
+
+    $('#number').text(randomNumber);
+    console.log(randomNumber);
+
+    // Register random numbers to each color rupee
+    $('#green-rupee').on('click', function() {
+        noise();
+        total = total + green;
+        console.log("New total= " + total);
+        $('#finalTotal').text(total);
+        if (total === randomNumber) {
+            console.log("nice");
+            wins++;
+            reset();
+        }
+        else if (total > randomNumber) {
+            console.log("you lose");
+            losses++;
+            reset();
+        }
+    })
+    $('#blue-rupee').on('click', function() {
+        noise();
+        total = total + blue;
+        console.log("New total= " + total);
+        $('#finalTotal').text(total);
+        if (total === randomNumber) {
+            console.log("nice");
+            wins++;
+            reset();
+        }
+        else if (total > randomNumber) {
+            console.log("you lose");
+            losses++;
+            reset();
+        }
+    })
+    $('#red-rupee').on('click', function() {
+        noise();
+        total = total + red;
+        console.log("New total= " + total);
+        $('#finalTotal').text(total);
+        if (total === randomNumber) {
+            console.log("nice");
+            wins++;
+            reset();
+        }
+        else if (total > randomNumber) {
+            console.log("you lose");
+            losses++;
+            reset();
+        }
+    })
+    $('#purple-rupee').on('click', function() {
+        noise();
+        total = total + purple;
+        console.log("New total= " + total);
+        $('#finalTotal').text(total);
+        if (total === randomNumber) {
+            console.log("nice");
+            wins++;
+            reset();
+        }
+        else if (total > randomNumber) {
+            console.log("you lose");
+            losses++;
+            reset();
+        }
+    });
+});
